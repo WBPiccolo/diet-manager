e9e89a8797f7474b005561d6a4ccc9457e51315e
francesco.piccolo <francesco.piccolo@lynxspa.com>
2023-02-25 12:07:00 +0100

    fix typo

--- electron-app.ts
+++ electron-app.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import Main from './electron-main';
-import Server from './elextron-express';
+import Server from './electron-express';
 
 Main.main(app, BrowserWindow);
 Server.startServer();
